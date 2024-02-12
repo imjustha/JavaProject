ad510f0e56ef54e3d0429f32f42989e14983f70c
imjustha <imjustha@gmail.com>
2024-02-11 23:22:58 -0600

    20240211 object orientation learn

---
+++ ObjectOrientation/src/Earth.java
@@ -0,0 +1,30 @@
+public class Earth {
+    public static void main(String[] args){
+//        Human tom;
+//        tom = new Human();
+//
+//        tom.age = 25;
+//        tom.eyeColor = "brown";
+//        tom.heightInInches = 72;
+//        tom.name = "Tom Zsaf";
+//        tom.speak();
+//
+//        Human hajeong = new Human();
+//        hajeong.speak()
+
+
+//        Human human1 = new Human();
+//        Human human2 = new Human();
+//        Human human3 = new Human();
+//
+//        human1.speak();
+//        human2.speak();
+//        human3.speak();
+
+        Human human1 = new Human("Tom", 25, 76, "Blue");
+        Human human2 = new Human("Joe", 28, 68, "Green");
+
+        human1.speak();
+        human2.speak();
+    }
+}
